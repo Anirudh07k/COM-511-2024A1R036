4770781e72a5186df2423ab6e9c6e4a835c27da2
Anirudh <2024a1r036@mietjammu.in>
2026-09-08 20:14:29 +0530

    Uploaded and updated files 08-09-26

--- 03-09-26 (2)/prog_1.py
+++ 03-09-26 (2)/prog_1.py
@@ -1,10 +1,12 @@
-# WAP to determine whether a student is eligible for a scholarship.
-# The scholarship should be granted if the student satifies either of the following conditions:
-# a) The student has a CGPA of 8.5 or above and attendence of 85 percent or above
-# b) The student has won a national-level compition
+"""
+WAP to determine whether a student is eligible for a scholarship.
+The scholarship should be granted if the student satifies either of the following conditions:
+a) The student has a CGPA of 8.5 or above and attendence of 85 percent or above
+b) The student has won a national-level compition
 
-# The program should take CGPA, attendence percentage and national-level competition status as input, 
-# then display whether the student is eligible for the scholarship
+The program should take CGPA, attendence percentage and national-level competition status as input, 
+then display whether the student is eligible for the scholarship
+"""
 
 CGPA = float(input("Enter CGPA : "))
 attendance = int(input("Enter attendence : "))
