a622ac31729cf2fd3bc1b23e7d25daa14d4d804e
Anirudh <2024a1r036@mietjammu.in>
2026-09-15 12:59:49 +0530

    Uploaded

--- 15-19-26/prog_12.py
+++ 15-19-26/prog_12.py
@@ -9,6 +9,6 @@ WAP to print an inverted right-angled triangle using stars.
 n = int(input("Enter val of N : "))
 
 for i in range(n):
-    for j in range(n):
-        print("* ", end = " ")
+    for j in range(n - i):
+        print("*", end = " ")
     print()
