c008e51f0aadcfae9d5b5e4dd06bb4895f8c0aca
Anirudh <2024a1r036@mietjammu.in>
2026-09-17 11:45:53 +0530

    uploaded files

--- 16-09-26 (2)/prog_4.py
+++ 16-09-26 (2)/prog_4.py
@@ -3,5 +3,12 @@ WAP to input a list of numbers and create a new list containing only unique elem
 """
 
 numbers = list(map(int , input("Enter Numbers : ").split()))
-x = set(numbers)
-print("Unique Numbers in List :",list(x))
+
+unique = []
+for i in numbers:
+    if i not in unique:
+        unique.append(i)
+print("Unique Numbers in List :",unique)
+
+# x = set(numbers)
+# print("Unique Numbers in List :",list(x))
