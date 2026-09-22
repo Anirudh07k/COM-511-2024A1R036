9d010f592eff430bce188d9e987b4c817f727244
Anirudh <2024a1r036@mietjammu.in>
2026-09-22 12:39:50 +0530

    Uploaded

--- size.py
+++ size.py
@@ -2,6 +2,11 @@ import sys
 
 print(sys.getsizeof(0))
 
-print(2 ** 63)
+# Size of long long in C++
+print(2 ** 31 - 1)
 
+# Size of long long in C++
+print(2 ** 63 - 1)
+
+# Size of int in Python
 print(2 ** 224)
